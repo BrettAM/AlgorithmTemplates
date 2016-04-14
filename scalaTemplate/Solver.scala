761beca51cffd53cfb268819a9c149d8c501e07d
Brett Menzies <scimor5@gmail.com>
2016-04-14 11:04:38 -0700

    Updated scala template adding output file

--- scalaTemplate/Solver.scala
+++ scalaTemplate/Solver.scala
@@ -1,9 +1,10 @@
 import scala.io.Source
+import java.io.PrintStream
 
 object Solver {
     type Input = Unit
     //return remaining input and one parsed out problem; t will not be Nil
-    def parseInput(t: List[String]): (List[String], Input) = {
+    def parseInput(t: Seq[String]): (Seq[String], Input) = {
         val first = t.head.split(' ').map(x => x.toInt)
         val input = ()
         (t.drop(1), input)
@@ -15,28 +16,33 @@ object Solver {
         //...
     }
 
-    def display(i :Int, a :Any) {
-        println("Case #"+i+": "+(a match {
+    def display(i :Int, a :Any, os: Seq[PrintStream]) {
+        for(s <- os){
+            s.print(s"Case #$i: ")
+            s.println(a match {
                 case d: Double => "%1.7f" format d
                 case a         => a.toString
-            }) )
+            })
+        }
+    }
+
+    def splitInput(l: Seq[String], v: Seq[Input]): Seq[Input] = {
+        if(l.length == 0) v.reverse
+        else {
+            val (remaining, input) = parseInput(l)
+            splitInput(remaining, input +: v)
+        }
     }
 
     def main(args: Array[String]) {
-        def splitInput(l: List[String], v: Seq[Input]): Seq[Input] = {
-            if(l == Nil) v
-            else {
-                val (remaining, input) = parseInput(l)
-                splitInput(remaining, input +: v)
-            }
-        }
         val ifname = if(args.length >= 1) args(0) else "input.txt"
+        val ofname = if(args.length >= 2) args(1) else "output.txt"
+        val outfs = Seq(System.out, new PrintStream(ofname))
         //dropped line is the "T test cases follow" line
-        val source = Source.fromFile(ifname).getLines().toList.drop(1)
-        //get input prepends as it recurses, resulting in a reversed list
-        val input  = splitInput(source, Nil).reverse
-        //change to "input.par" if you are feeling adventurous
-        val output = input.map(problem => doProblem(problem))
-        output.seq.zipWithIndex foreach { case(a,i) => display(i+1, a) }
+        val source = Source.fromFile(ifname).getLines().drop(1).toSeq
+        val input  = splitInput(source, Nil)
+        //change to "input.par" to try parallel execution
+        val output = input.map(doProblem)
+        output.zipWithIndex foreach { case(a,i) => display(i+1, a, outfs) }
     }
 }
