111cb2c85fc250a8817444bf6b4aa1a60ec4461c
Brett Menzies <scimor5@gmail.com>
2016-04-25 11:04:35 -0700

    A few more snippents

---
+++ codebook/Snippets.scala
@@ -0,0 +1,20 @@
+object Snippets{
+    /** create a map of unique values to sequence of all occurances
+      * groupBy Partitions this traversable collection into a map of traversable
+      *   collections according to some discriminator function
+      */
+    seq.groupBy(identity)
+
+    /** Do a binary search between l and r
+      * Finds the lowest value where lt == false
+      */
+    def search(l: Int, r: Int)(implicit lt: Int=>Boolean):Int = {
+        val m = (l+r)/2
+        println(s"Looking at $m between $l and $r")
+        if(m == l || m == r) m
+        else {
+            if(lt(m)) search(l, m)
+            else search(m, r)
+        }
+    }
+}
